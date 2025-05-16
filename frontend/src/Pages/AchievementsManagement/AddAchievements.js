2e9ad16348c8bf84be963fa97964445e2cd1594c
IT22371904SLIIT <kavindu2002tcc@gmail.com>
2025-05-16 09:31:02 +0530

    error fixed

--- frontend/src/Pages/AchievementsManagement/AddAchievements.js
+++ frontend/src/Pages/AchievementsManagement/AddAchievements.js
@@ -195,7 +195,7 @@ function AddAchievements() {
                 <option value="Street Food">Organic Fertilizer</option>
                 <option value="One-Pot Meals">Inorganic Fertilizer</option>
                 <option value="Meal Prep / Batch Cooking">Compost</option>
-                <option value="Budget-Friendly">Manure</option>
+                <option value="Budget-Friendly">Plants</option>
                 <option value="Kid-Friendly">Green Manure</option>
                 <option value="Healthy Recipes">Biofertilizer</option>
                 <option value="Comfort Food">Nitrogen Fertilizer</option>
