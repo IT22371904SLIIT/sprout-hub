88f9791b26f293dbc1e99b3846d0d0c6be1dfe4b
Dinuwarabandara5 <dinuwarabandara5@gmail.com>
2025-05-16 12:43:12 +0530

    post del

--- frontend/src/Pages/PostManagement/AllPost.js
+++ frontend/src/Pages/PostManagement/AllPost.js
@@ -93,7 +93,7 @@ function AllPost() {
 
     try {
       await axios.delete(`http://localhost:8080/posts/${postId}`);
-      alert('Post deleted successfully!');
+      alert('Deleted the post successfully!');
       setPosts(posts.filter((post) => post.id !== postId));
       setFilteredPosts(filteredPosts.filter((post) => post.id !== postId));
     } catch (error) {
