1deb9f0b9bf9e62137caa445a9bd4ef00b0fc68d
Dinuwarabandara5 <dinuwarabandara5@gmail.com>
2025-05-18 03:50:19 +0530

    comment

--- frontend/src/Pages/PostManagement/AllPost.js
+++ frontend/src/Pages/PostManagement/AllPost.js
@@ -166,7 +166,7 @@ function AllPost() {
   const handleAddComment = async (postId) => {
     const userID = localStorage.getItem('userID');
     if (!userID) {
-      alert('Please log in to comment.');
+      alert('Please log in to comment to a post.');
       return;
     }
     const content = newComment[postId] || '';
